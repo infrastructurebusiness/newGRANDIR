54f84ceaf3c567b78347575e8c526b5ed9f462ad
infrastructurebusiness <infrastructure.business.r@gmail.com>
2026-09-16 19:56:44 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,1 @@
+const io=new IntersectionObserver((entries)=>{entries.forEach(e=>{if(e.isIntersecting)e.target.classList.add("visible")})},{threshold:.08});document.querySelectorAll(".reveal").forEach(el=>io.observe(el));
